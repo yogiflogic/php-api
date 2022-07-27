836ae381aa273ccda4c1d7b313c6bb7e4ea2ddab
yogiflogic <yogiflogic79@gmail.com>
2022-07-27 11:05:06 +0700

    Menghapus API ID yang terpublikasi

--- api.php
+++ api.php
@@ -1,11 +1,11 @@
 <?php
 
 //api openweathermap
-$APPID = "746422cd5541efc4f524ab4bc115c58a";
+$APPID = "Isi Dengan API ID";
 $APIurl = "https://api.openweathermap.org/data/2.5/weather?q=Jakarta&appid=$APPID";
 
 // api youtube
-$APPIDYT = "AIzaSyD8qSzPPyFJyPugifji4rc3FcLT8rP5XNA";
+$APPIDYT = "Isi Dengan API ID";
 $APIyt = "https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UCSUWBtaz8MVTFy9GULySSmA&key=$APPIDYT";
 
 $json = file_get_contents($APIurl);
